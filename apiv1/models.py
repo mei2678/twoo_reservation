b165cc65600a6cbafd3cc3eea5dd25cbc0fd3b11
mei sakata <sakata952m@gmail.com>
2023-09-30 21:31:58 +0900

    プロジェクト削除

--- apiv1/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-
-class TestData(models.Model):
-    testname = models.CharField(max_length=100)
-    testvalue = models.IntegerField()
--- apiv1/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import TestData
-from .serializers import TestDataSerializer
-
-
-class TestDataViewSet(viewsets.ModelViewSet):
-    queryset = TestData.objects.all()
-    serializer_class = TestDataSerializer
